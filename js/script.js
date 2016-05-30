892912dad27b4262fc2c71285badc42aa909f723
Nate <npaulemon@gmail.com>
2016-05-30 17:48:04 -0400

    navbar jquery scroll added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+	// navbar toogle
 	$('#navbar-button').click(function(){
 	    $('#myNavbar').slideToggle('fast');
 	});
@@ -7,5 +9,22 @@ $(document).ready(function(){
 		$('#myNavbar').slideToggle('fast');
 	});
 
+	// height adjustment for content in accordion
     $( "#accordion" ).accordion({heightStyle:'content'});
+
+    // navbar slide to content
+    $('#servicesLink').click(function() {
+		$("html, body").animate({ scrollTop:$("#services-lead-header").offset().top -45 }, "slow");
+		return false;
+	});
+
+    $('#aboutLink').click(function() {
+		$("html, body").animate({ scrollTop:$("#about-lead-header").offset().top -45 }, "slow");
+		return false;
+	});
+
+	$('#contactLink').click(function() {
+		$("html, body").animate({ scrollTop:$("#contactRow").offset().top -45 }, "slow");
+		return false;
+	});
 });
