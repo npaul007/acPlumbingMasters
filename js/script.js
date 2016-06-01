d1a05810d5d11bc9de3e5ad6ab3fee54e4ca374c
Nate <npaulemon@gmail.com>
2016-05-31 20:59:16 -0400

    began refractoring jquery scroll code

--- js/script.js
+++ js/script.js
@@ -14,29 +14,34 @@ $(document).ready(function(){
 
     // navbar slide to content
     $('#servicesLink').click(function() {
-		$("html, body").animate({ scrollTop:$("#services-lead-header").offset().top -45 }, "slow");
-		return false;
+		//$("html, body").animate({ scrollTop:$("#services-lead-header").offset().top -45 }, "slow");
+		scrollTo('#services-lead-header');
 	});
 
     $('#aboutLink').click(function() {
-		$("html, body").animate({ scrollTop:$("#about-lead-header").offset().top -45 }, "slow");
-		return false;
+		scrollTo("#about-lead-header");
 	});
 
 	$('#contactLink').click(function() {
-		$("html, body").animate({ scrollTop:$("#contactRow").offset().top -45 }, "slow");
-		return false;
+		scrollTo("#contactRow");
 	});
 
 	$('.navbar-brand').click(function() {
-		$("html, body").animate({ scrollTop:0 }, "slow");
-		return false;
+		//$("html, body").animate({ scrollTop:0 }, "slow");
+		scrollTo(0);
 	});
 
 	$('#certifLink').click(function() {
-		$("html, body").animate({ scrollTop:$('#nest-lead-header').offset().top - 45 }, "slow");
-		return false;
+		scrollTo('#nest-lead-header');
 	});
 
-
 });
+
+function scrollTo(dest){
+	if(isNaN(dest))
+		$("html, body").animate({ scrollTop:$(dest).offset().top -45 }, "slow");
+	else
+		$("html, body").animate({ scrollTop:dest }, "slow");
+
+	return false;
+}
