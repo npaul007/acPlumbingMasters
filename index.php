7e19ee2d18b34afcf0fbf5f70566823217d6b2fd
Nate <npaulemon@gmail.com>
2016-11-21 20:12:24 -0500

    lead row reformatted/centered using flex

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ My portfolio: https://github.com/npaul007
 		        <span class="icon-bar"></span>
 		        <span class="icon-bar"></span> 
 		      </button>
-		      <a class="navbar-brand"><strong>ACPlumbingMasters</strong></a>
+		      <a class="navbar-brand"><strong>Lebrun's HVAC Services</strong></a>
 		    </div>
 		    <div class="collapse navbar-collapse" id="myNavbar">
 		      <ul class="nav navbar-nav navbar-right">
@@ -55,15 +55,13 @@ My portfolio: https://github.com/npaul007
 		</nav>
 
 		<div class="container-fluid" id="wrapper">
+				<img src="images/meter.png" class="img-responsive" id="meterImg" alt="">
+
+
 			<div class="row" id="lead-row">
 				<div class="container-fluid">
 					<div class="row">
-						<div class="col-md-12">
-							<img src="images/meter.png" class="img-responsive center" id="meterImg" alt="">
-						</div>
-					</div>
-					<div class="row" id="sub-lead-row">
-						<div class="col-md-12">
+						<div class="col-lg-12">
 							<h1 class="center-text lead-header" id="lead-header"><strong>LeBrun's HVAC Services. LLC</strong></h1>
 							<h2 class="center-text" id="slogan">"<strong>HVAC Comfort Always at a Comfortable Price!"</strong></h2>
 							<h4 class="center-text"><span style="color:red">Heating</span>, 
@@ -87,7 +85,7 @@ My portfolio: https://github.com/npaul007
 
 			<div class="row oddRow">
 				<div class="container">
-					<div class="col-md-4">
+					<div class="col-lg-4">
 						<img src="images/1464235634_Icon_Element_Fire_03.png" class="center icon-png" alt="">
 						<h2 class="center-text">Heating</h2>
 						<p>Most modern heating units, whether it be  gas furnaces or heat pumps, are HVAC systems that draw air in from the living space, heats it up and returns it to warm the home. 
@@ -98,7 +96,7 @@ My portfolio: https://github.com/npaul007
 						Contact our hvac specialist for your furnace repair and maintenance needs or to find out what type of furnace best fits your home.</p>
 						</p>
 					</div>
-					<div class="col-md-4">
+					<div class="col-lg-4">
 						<img src="images/1464227363_fan.png" class="center icon-png" alt="">
 						<h2 class="center-text">Air Conditioning</h2>
 						<p>Most modern A/C units whether it be straight A/C or heat pumps, are HVAC systems that draw air in warm air from the living space, cools it down and returns it to cool down the home. </p>
@@ -106,7 +104,7 @@ My portfolio: https://github.com/npaul007
 						<p>
 						Contact our hvac specialist for your furnace repair and maintenance needs or to find out what type of furnace best fits your home.</p>
 					</div>
-					<div class="col-md-4">
+					<div class="col-lg-4">
 						<img src="images/1464227305_Plumbing.png" class="center icon-png" alt="">
 						<h2 class="center-text">Diagnostics & Repairs</h2>
 						<p>
@@ -119,17 +117,17 @@ My portfolio: https://github.com/npaul007
 			
 			<div class="row evenRow">
 				<div class="container">
-					<div class="col-md-6">
+					<div class="col-lg-6">
 						<h1 class="center-text"><strong>We also offer:</strong></h1>
 						<br/>
 						<div id="accordion">
-						  <h2><img src="images/meter.png" class="accordionMetoerImg" alt="">Maintenance Contracts</h2>
+						  <h2><img src="images/meter.png" class="accordionMetoerImg" alt=""><strong>Maintenance Contracts</strong></h2>
 						  <div>
 						    <p>
 						   To help extend the life of your furnace be sure to provide regular maintenance.  We offer affordable yearly service contract for residential & light commercial customers. Sign up by calling us or making an appointment today!
 						    </p>
 						  </div>
-						  <h2><img src="images/meter.png" class="accordionMetoerImg" alt="">HVAC Unit Protection</h2>
+						  <h2><img src="images/meter.png" class="accordionMetoerImg" alt=""><strong>HVAC Unit Protection</strong></h2>
 						  <div>
 						    <p>
 						  Each year , the average home or business experiences more than 300 potentially damaging surges.Today's Heating, Ventilation and Air Conditioning (HVAC) Equipment is becoming more sensitive to this avoidable environmental condition.</p>
@@ -139,7 +137,7 @@ My portfolio: https://github.com/npaul007
 							Installation of a quality surge protector product to prevent or minimize the damage to your HVAC units. It removes the daily power surges and spikes from the local power company, it protects from lightning strikes and the life of your equipment as well. Get it installed today by calling us for an appointment now! 
 							</p>
 						  </div>
-						  <h2><img src="images/meter.png" class="accordionMetoerImg" alt="">Water Damage Protection</h2>
+						  <h2><img src="images/meter.png" class="accordionMetoerImg" alt=""><strong>Water Damage Protection</strong></h2>
 						  <div>
 							<p>The damage caused by excess condensation in your HVAC system is preventable. We offer brand names moisture prevention systems for your air conditioner, which monitors AC condensation and warns users when the level of condensation is unsafe.</p>
 							<p>Some of their caracteristics and benefits:</p>
@@ -152,7 +150,7 @@ My portfolio: https://github.com/npaul007
 								<li>Excellent for Rooftop Package Units</li>
 							</ul>
 						  </div>
-						  <h2><img src="images/meter.png" class="accordionMetoerImg" alt="">New Unit Install  & Upgrades</h2>
+						  <h2><img src="images/meter.png" class="accordionMetoerImg" alt=""><strong>New Unit Install  & Upgrades</strong></h2>
 						  <div>
 						    <p>
 								Reduce Energy Costs &
@@ -166,14 +164,16 @@ My portfolio: https://github.com/npaul007
 						<br/>
 					</div>
 
-					<div class="col-md-6">
-						<h1 class="center-text"><strong>Current Promotions</strong></h1>
-						<br/>
-						<ul>
-							<li><p>FREE ESTIMATE for your new AC or Heating System</p></li>
-							<li><p>Save 10% on your next HVAC Service when you make a recommendation</p></li>
-						</ul>
+					<div class="col-lg-1"></div>
+
+					<div class="col-lg-5 center-text">
+						<h1><strong>Current Promotions</strong></h1>
+		
 						<img src="images/piggy-bank-2-xxl.png" class="img-responsive center" alt=""/>
+						<br/>
+						<p>FREE ESTIMATE for your new AC or Heating System</p>
+						<p>Save 10% on your next HVAC Service when you make a recommendation</p>
+						
 					</div>
 				</div>
 			</div>
@@ -190,8 +190,8 @@ My portfolio: https://github.com/npaul007
 
 			<div class="row evenRow">
 				<div class="container">
-					<div class="col-md-4">
-						<h3><strong>Heating and Air Conditioning</strong></h3>
+					<div class="col-lg-7">
+						<h3><strong>Heating & Air Conditioning</strong></h3>
 						<ul>
 							<li>Just as a tune-up for your car can improve your gas mileage, a yearly tune-up of your heating and cooling system can improve efficiency and comfort.
 							</li>
@@ -208,14 +208,6 @@ My portfolio: https://github.com/npaul007
 							Call us today to get  one of our trained Indoor Air Quality (IAQ)  specialists to show you how you and your family could breathe comfortably and safely.
 							</li>
 						</ul>
-						<h3><strong>Diagnostic & Repairs</strong></h3>
-						<ul>
-							<li>HVAC systems break down. Don't panic! Call us and we will quickly diagnose your system to pin point the problems then making the appropriate recommendations to fix the problems.
-							</li>
-						</ul>
-					</div>
-
-					<div class="col-md-4">
 						<h3><strong>Maintenance Contracts</strong></h3>
 						<ul>
 							<li>
@@ -226,30 +218,6 @@ My portfolio: https://github.com/npaul007
 								prevent costly breakdowns and disasters and
 								Improve system safety.
 								Sign Up for Home Service Plan today!
-							</li>
-						</ul>
-						<h3><strong>HVAC Unit Protection</strong></h3>
-						<ul>
-							<li>
-								DON'T LEAVE YOUR AIR CONDITIONER DEFENSELESS !
-							</li>
-							<li>
-							Daily power surges from your utility and lightning strikes could badly damage your HVAC equipment.
-							Professionally  Installed Surge Protection:
-							Protects Against Lightning
-							Protects against Utility Grid "Spikes"and Extends the Life of your equipment.
-							</li>
-						</ul>
-					</div>
-
-					<div class="col-md-4">
-						<h3><strong>Water Damage Protection</strong></h3>
-						<ul>
-							<li>As your A/C is cooling down your house, it's also removing humidity in the process. That humidity condensates and then that water drains down through the PVC drain system. What happens is that over time, the PVC drain  clogs up, 
-								causing dirty water to overflow potentially damaging some electrical part(s), the floor or the ceilling.</li>
-							<li>
-								 We offer two kinds. The first one will make an "alarming" sound when it comes in contact with water so you could turn off the unit while the other will actually turn off the unit itself in the event that the PVC drain clogged up. Are you protected ? 
-								 Get the peace of mind by calling us today! 
 							</li>
 						</ul>
 						<h3><strong>New Unit Install & Upgrades</strong></h3>
@@ -263,6 +231,35 @@ My portfolio: https://github.com/npaul007
 								Installed with the proper amount of refrigerant
 							</li>
 							<li>Call now for an estimate!</li>
+						</ul>
+						<h3><strong>Diagnostic & Repairs</strong></h3>
+						<ul>
+							<li>HVAC systems break down. Don't panic! Call us and we will quickly diagnose your system to pin point the problems then making the appropriate recommendations to fix the problems.
+							</li>
+						</ul>
+					</div>
+
+					<div class="col-lg-5">
+						<h3><strong>Water Damage Protection</strong></h3>
+						<ul>
+							<li>As your A/C is cooling down your house, it's also removing humidity in the process. That humidity condensates and then that water drains down through the PVC drain system. What happens is that over time, the PVC drain  clogs up, 
+								causing dirty water to overflow potentially damaging some electrical part(s), the floor or the ceilling.</li>
+							<li>
+								 We offer two kinds. The first one will make an "alarming" sound when it comes in contact with water so you could turn off the unit while the other will actually turn off the unit itself in the event that the PVC drain clogged up. Are you protected ? 
+								 Get the peace of mind by calling us today! 
+							</li>
+						</ul>
+						<h3><strong>HVAC Unit Protection</strong></h3>
+						<ul>
+							<li>
+								DON'T LEAVE YOUR AIR CONDITIONER DEFENSELESS !
+							</li>
+							<li>
+							Daily power surges from your utility and lightning strikes could badly damage your HVAC equipment.
+							Professionally  Installed Surge Protection:
+							Protects Against Lightning
+							Protects against Utility Grid "Spikes"and Extends the Life of your equipment.
+							</li>
 						</ul>
 					</div>
 				</div>
@@ -281,10 +278,10 @@ My portfolio: https://github.com/npaul007
 
 			<div class="row evenRow">
 				<div class="container">
-					<div class="col-md-4">
+					<div class="col-lg-4">
 						<img src="images/nicsonPortrait.jpg" class="img-responsive img-circle center" alt="">
 					</div>
-					<div class="col-md-8" id="ceo-text">
+					<div class="col-lg-8" id="ceo-text">
 						<h2><strong>Nicson Lebrun</strong></h2>
 						<h3>Chief Executive Officer of Lebrun's HVAC Services LLC.</h3>
 						<p>
@@ -301,7 +298,7 @@ My portfolio: https://github.com/npaul007
 				<div class="container">
 					<br/>
 					<article>
-						<div class="col-md-5">
+						<div class="col-lg-5">
 							<p class="nestpro-text">We are also an <strong>Approved</strong>  NEST PRO Installer in the DC Metro Area !</p>
 							<p class="nestpro-text">Click this  video to see how Nest products could save you more than just money !</p>
 							<br/>
@@ -309,7 +306,7 @@ My portfolio: https://github.com/npaul007
 								<img src="images/nest.jpg" class="img-responsive" id="nestImg"  alt="">
 							</a>
 						</div>
-						<div class="col-md-7">
+						<div class="col-lg-7">
 							<div class="video-container">
 							  <iframe width="300" height="168" src="http://www.youtube.com/embed/5thQRIX3Rio" frameborder="0" allowfullscreen></iframe>
 							</div>
@@ -325,21 +322,31 @@ My portfolio: https://github.com/npaul007
 			</div>
 
 			<div class="row evenRow" id="contactRow">
-				<div class="col-md-4">
+				<div class="container">
+				<div class="col-lg-5 center-text">
 					<h1 class="lead-header"><strong>Contact Us</strong></h1>
-					<h3><strong>tel:</strong><a href="tel:301-613-1392"> +301-613-1392</a></h3>
-					<h3><strong>fax:</strong> +301-497-1945</h3>
-					<h3><strong>email:</strong><a href="mailto:prstech1@gmail.com"> prstech1@gmail.com</a> </h3>
+					<h3><strong>tel:</strong></h3>
+					
+					<h4><a href="tel:301-613-1392"> +301-613-1392</a></h4>
+					
+					<h3><strong>fax:</strong></h3>
+					
+					<h4>+301-497-1945</h4>
+					
+					<h3><strong>email:</strong></h3>
+					
+					<h4><a href="mailto:prstech1@gmail.com"> prstech1@gmail.com</a></h4>
+					<br/>
 				</div>
 
-				<div class="col-md-8">
+				<div class="col-lg-7">
 					<div class="row">
 						<form  class="center-text" action="index.php" method="post" id="messageForm">			
-							<div class="col-md-6">
+							<div class="col-lg-6">
 								<input type="text"  name="firstName" placeholder="your first name" class="text-input center form-control" required>
 								<input type="text"  name="lastName" placeholder="your last name" class="text-input center form-control" required>
 							</div>
-							<div class="col-md-6">
+							<div class="col-lg-6">
 								<input type="email"  name="email" placeholder="your e-mail" class="text-input center form-control" required>
 								<input type="text"  name="phoneNumber" placeholder="your phone number" class="text-input center form-control" required>
 							</div>
@@ -348,7 +355,7 @@ My portfolio: https://github.com/npaul007
 					</div>
 
 					<div class="row">
-						<div class="col-md-12">
+						<div class="col-lg-12">
 							<textarea name="message" form="messageForm" cols="30" rows="10" class="text-input center form-control" placeholder="your message" required></textarea>
 
 							<br/>
@@ -357,6 +364,7 @@ My portfolio: https://github.com/npaul007
 
 						</div>
 					</div>
+				</div>
 				</div>
 			</div>
 
@@ -370,7 +378,7 @@ My portfolio: https://github.com/npaul007
 			</div>
 
 			<footer class="row footer">
-				<div class="container">
+				<div class="container-fluid">
 					<div class="inline-block">
 						<p>&copy Lebrun's HVAC Services LLC</p>
 					</div>
