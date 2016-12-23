385216b134908aaed15e81e055f5ed64f591f5c9
Nathanael <npaulemon@gmail.com>
2016-12-22 21:42:27 -0500

    commit of updated site

---
+++ index.php
@@ -0,0 +1,428 @@
+<!-- 
+Site Created by : Nathanael Auxil Paulemon
+For Business purposes contact me at : npaulemon@gmail.com or (203) - 820 - 5307
+My portfolio: https://github.com/npaul007
+-->
+<html>
+	<head>
+		<title>Lebrun's HVAC Services LLC. Laurel, MD 20723</title>
+
+		<meta name="description" content="Lebrun's HVAC Services provides cooling, heating, 
+		and Indoor Air Quality/Air Conditioning services with a knowledgeable team of trained specialist(s) 
+		who have the professional background to get the job done right. Garanteed. We are the 
+		AC Plumbing Masters. Yours truly Nicson Lebrun. We are servicing Maryland, Viriginia and D.C" />
+
+		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<script src="js/jquery/jquery-2.2.4.js"></script>
+
+		<link rel="stylesheet" href="js/jquery-ui/jquery-ui.css">
+		<script src="js/jquery-ui/jquery-ui.js"></script>
+
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		
+		<link rel="stylesheet" href="css/bootstrap-theme.css">
+		
+		<script src="js/script.js"></script>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-default navbar-fixed-top">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle btn btn-default" id="navbar-button" data-toggle="collapse" data-target="#myNavbar">
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span> 
+		      </button>
+		      <a class="navbar-brand"><strong>Lebrun's HVAC Services</strong></a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="myNavbar">
+		      <ul class="nav navbar-nav navbar-right">
+		        <li id="servicesLink"><a>Services</a></li>
+		        <li id="certifLink"><a>Recommendations</a></li>
+		        <li id="aboutLink"><a>About</a></li>
+		        <li id="contactLink"><a>Contact</a></li> 
+		      </ul>
+		    </div>
+		  </div>
+		</nav>
+
+		<div class="container-fluid" id="wrapper">
+				<img src="images/meter.png" class="img-responsive" id="meterImg" alt="">
+
+
+			<div class="row" id="lead-row">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-lg-12">
+							<h1 class="center-text lead-header" id="lead-header"><strong>LeBrun's HVAC Services. LLC</strong></h1>
+							<h2 class="center-text" id="slogan">"<strong>HVAC Comfort Always at a Comfortable Price!"</strong></h2>
+							<h4 class="center-text"><span style="color:red">Heating</span>, 
+								<span style="color:blue">Cooling</span> & <span style="color:green">Indoor Air Quality</span> </h4>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			
+			<div class="row" id="services-lead-header">
+				<div class="container">
+					<header>
+						<h1 class="center-text lead-header"><strong>Services</strong></h1>
+					</header>
+					<article class="center" id="services-par">
+						<p >We put HVAC comfort at your reach by offering a complete range of HVAC services from preventive maintenance to installation of new unit(s). Take a look below at the services we offer.</p>
+					</article>
+				</div>
+			</div>
+
+			<div class="row oddRow">
+				<div class="container">
+					<div class="col-lg-4">
+						<img src="images/1464235634_Icon_Element_Fire_03.png" class="center icon-png" alt="">
+						<h2 class="center-text">Heating</h2>
+						<p>Most modern heating units, whether it be  gas furnaces or heat pumps, are HVAC systems that draw air in from the living space, heats it up and returns it to warm the home. 
+
+						<p>Modern furnaces are forced air systems in three basic configurations based on the direction in which the furnace outputs heat: Those units could be installed as upflow unit, horizontal unit or downflow unit.</p>
+
+						<p>  
+						Contact our hvac specialist for your furnace repair and maintenance needs or to find out what type of furnace best fits your home.</p>
+						</p>
+					</div>
+					<div class="col-lg-4">
+						<img src="images/1464227363_fan.png" class="center icon-png" alt="">
+						<h2 class="center-text">Air Conditioning</h2>
+						<p>Most modern A/C units whether it be straight A/C or heat pumps, are HVAC systems that draw air in warm air from the living space, cools it down and returns it to cool down the home. </p>
+						<p>Modern A/C systems are forced air systems in three basic configurations based on the direction in which the air handling unit outputs the cooled air: Those units could be installed as upflow unit, horizontal unit or downflow unit. </p>
+						<p>
+						Contact our hvac specialist for your furnace repair and maintenance needs or to find out what type of furnace best fits your home.</p>
+					</div>
+					<div class="col-lg-4">
+						<img src="images/1464227305_Plumbing.png" class="center icon-png" alt="">
+						<h2 class="center-text">Diagnostics & Repairs</h2>
+						<p>
+						Whatever type of heating you have, it's a complex system. And while it's designed to work efficiently, after years of service it can break down and you may need heating system repair.  </p>
+						<p>The good news is our troubleshooting expert(s) will quickly find and solve the problem(s). So, is your place too hot, too cold, or too humid or too dry ? We will find the cause(s) and make yoour house the comfotable place that deserve.</p>
+						<p> Call us today for your diagnostic and all your HVAC repairs needs!</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="row evenRow">
+				<div class="container">
+					<div class="col-lg-6">
+						<div class="icon-png center ">
+							<i class="fa fa-tint"></i>
+						</div>
+						<h2 class="center-text">Water Damage Protection</h2>
+							<p>The damage caused by excess condensation in your HVAC system is preventable. We offer brand names moisture prevention systems for your air conditioner, which monitors AC condensation and warns users when the level of condensation is unsafe.</p>
+							<p>Some of their characteristics and benefits:</p>
+							<ul>
+								<li>Monitor AC condensation</li>
+								<li>Prevent condensation overflow on 24V air conditioning systems</li>
+								<li>Avoid moisture damage caused by AC condensation overflow</li>
+								<li>Residential</li>
+								<li>Commercial</li>
+								<li>Excellent for Rooftop Package Units</li>
+							</ul>
+					</div>
+					<div class="col-lg-6">
+						<div class="icon-png center ">
+							<i class="fa fa-shield"></i>
+						</div>
+						<h2 class="center-text">HVAC Unit Protection</h2>
+						    <p>
+						  Each year , the average home or business experiences more than 300 potentially damaging surges.Today's Heating, Ventilation and Air Conditioning (HVAC) Equipment is becoming more sensitive to this avoidable environmental condition.</p>
+						  <p>
+							What can be done?</p>
+							<p>
+							Installation of a quality surge protector product to prevent or minimize the damage to your HVAC units. It removes the daily power surges and spikes from the local power company, it protects from lightning strikes and the life of your equipment as well. Get it installed today by calling us for an appointment now! 
+							</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="row oddRow">
+				<div class="container">
+					<div class="col-lg-6">
+						<div class="icon-png center">
+							<i class="fa fa-handshake-o" aria-hidden="true"></i>
+						</div>
+						<h2 class="center-text">Maintenance Contracts</h2>
+						<p>To help extend the life of your furnace be sure to provide regular maintenance.</p>
+						<p>We offer affordable yearly service contract for residential & light commercial customers.</p>
+						<p>Sign up by calling us or making an appointment today!</p>
+					</div>
+					<div class="col-lg-6">
+						<div class="icon-png center">
+							<i class="fa fa-wrench" aria-hidden="true"></i>
+						</div>
+						<h2 class="center-text">New Unit Install  & Upgrades</h2>
+					    <p>
+							Reduce Energy Costs &
+							Reduce Pollution by Updating Your Heating and Cooling System.
+							If your system is older, or you  need a new unit you would the opportunity to cut your fuel bills and your system's pollution output in half with a new, energy efficient air conditioning and heating system. </p>
+
+							<p>Contact one our comfort advisers near you to schedule a no-cost analysis of the benefits and savings of replacing your system with a new energy efficient air conditioning and heating system. 
+					    </p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row evenRow">
+				<div class="container">
+					<div class="col-lg-12 center-text">
+						<h1><strong>Current Promotions</strong></h1>
+		
+						<img src="images/piggy-bank-2-xxl.png" class="img-responsive center" alt=""/>
+						<br/>
+						<p>FREE ESTIMATE for your new AC or Heating System</p>
+						<p>Save 10% on your next HVAC Service when you make a recommendation</p>
+						<br/>
+						<br/>
+					</div>
+				</div>
+			</div>
+  			<div class="row" id="rec-header">
+				<div class="container">
+					<header>
+						<h1 class="lead-header center-text"><strong>Recommendations</strong></h1>
+						<article class="center" id="rec-par">				
+							<p >Here are some tips to help create a comfortable environment for your home or business:</p>
+						</article>					
+					</header>
+				</div>
+			</div>
+
+			<div class="row evenRow">
+				<div class="container">
+					<div class="col-lg-7">
+						<h3><strong>Heating & Air Conditioning</strong></h3>
+						<ul>
+							<li>Just as a tune-up for your car can improve your gas mileage, a yearly tune-up of your heating and cooling system can improve efficiency and comfort.
+							</li>
+							<li>
+							You should tune up your HVAC unit yearly
+							Call us today for your Fall heating check up or for your Spring A/C check up.
+							</li>
+						</ul>
+						<h3><strong>Indoor Air Quality Solutions</strong></h3>
+						<ul>
+							<li>Indoor Air pollutants can aggravate asthma and allergies and other respitory problems . Installing a whole house air cleaner on your ductwork will purify the air that you and your family breathe. 
+							</li>
+							<li>
+							Call us today to get  one of our trained Indoor Air Quality (IAQ)  specialists to show you how you and your family could breathe comfortably and safely.
+							</li>
+						</ul>
+						<h3><strong>Maintenance Contracts</strong></h3>
+						<ul>
+							<li>
+								They help Improve system performance,
+								Increase energy efficiency,
+								Reduce energy costs,
+								Extend equipment lifespan,
+								prevent costly breakdowns and disasters and
+								Improve system safety.
+								Sign Up for Home Service Plan today!
+							</li>
+						</ul>
+						<h3><strong>New Unit Install & Upgrades</strong></h3>
+						<ul>
+							<li>
+								We ensure that your new equipment is:
+ 
+								Correctly sized to meet your home's needs
+								Connected to a well-sealed duct system
+								Operating with sufficient airflow in the system
+								Installed with the proper amount of refrigerant
+							</li>
+							<li>Call now for an estimate!</li>
+						</ul>
+						<h3><strong>Diagnostic & Repairs</strong></h3>
+						<ul>
+							<li>HVAC systems break down. Don't panic! Call us and we will quickly diagnose your system to pin point the problems then making the appropriate recommendations to fix the problems.
+							</li>
+						</ul>
+					</div>
+
+					<div class="col-lg-5">
+						<h3><strong>Water Damage Protection</strong></h3>
+						<ul>
+							<li>As your A/C is cooling down your house, it's also removing humidity in the process. That humidity condensates and then that water drains down through the PVC drain system. What happens is that over time, the PVC drain  clogs up, 
+								causing dirty water to overflow potentially damaging some electrical part(s), the floor or the ceilling.</li>
+							<li>
+								 We offer two kinds. The first one will make an "alarming" sound when it comes in contact with water so you could turn off the unit while the other will actually turn off the unit itself in the event that the PVC drain clogged up. Are you protected ? 
+								 Get the peace of mind by calling us today! 
+							</li>
+						</ul>
+						<h3><strong>HVAC Unit Protection</strong></h3>
+						<ul>
+							<li>
+								DON'T LEAVE YOUR AIR CONDITIONER DEFENSELESS !
+							</li>
+							<li>
+							Daily power surges from your utility and lightning strikes could badly damage your HVAC equipment.
+							Professionally  Installed Surge Protection:
+							Protects Against Lightning
+							Protects against Utility Grid "Spikes"and Extends the Life of your equipment.
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div class="row evenRow" id="about-lead-header">
+				<div class="container">
+					<header>
+						<h1 class="lead-header center-text"><strong>About Us</strong></h1>
+						<article class="center"  id="aboutus-par">
+							<p>Lebrun's HVAC Services provides cooling, heating, and Indoor Air Quality services  with a knowledgeable team of trained specialist(s) who have the professional background to get the job done right. Guaranteed.</p>
+						</article>
+					</header>
+				</div>
+			</div>
+
+			<div class="row evenRow">
+				<div class="container">
+					<div class="col-lg-4">
+						<img src="images/nicsonPortrait.jpg" class="img-responsive img-circle center" alt="">
+					</div>
+					<div class="col-lg-8" id="ceo-text">
+						<h2><strong>Nicson Lebrun</strong></h2>
+						<h3>Chief Executive Officer of Lebrun's HVAC Services LLC.</h3>
+						<p>
+						You can count on our courteous technician(s), and Comfort Advisor(s) to provide you with exceptional service - 24/7.</p>
+						<p>
+						When our technician(s) visit(s) your home, rest assured that they have passed national background and drug tests, and are equipped to provide you with the service you need.</p>
+						<p>
+						We treat you and your property with respect by calling before we arrive at your property, providing an honest, upfront estimate you approve before any work is done, and taking care of your property by using shoe covers and drop cloths as needed.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="row oddRow" >
+				<div class="container">
+					<br/>
+					<article>
+						<div class="col-lg-5">
+							<p class="nestpro-text">We are also an <strong>Approved</strong>  NEST PRO Installer in the DC Metro Area !</p>
+							<p class="nestpro-text">Click this  video to see how Nest products could save you more than just money !</p>
+							<br/>
+							<a href="https://nest.com/" target="_blank" class="nestImg-container">
+								<img src="images/nest.jpg" class="img-responsive" id="nestImg"  alt="">
+							</a>
+						</div>
+						<div class="col-lg-7">
+							<div class="video-container">
+							  <iframe width="300" height="168" src="http://www.youtube.com/embed/5thQRIX3Rio" frameborder="0" allowfullscreen></iframe>
+							</div>
+						</div>
+					</article>
+				</div>
+			</div>
+
+			<div class="row" id="mapRow">
+				<div class="container">
+					<h1 class="center-text" id="servicing-text"><strong>Servicing Maryland, Virginia & D.C</strong></h1>
+				</div>
+			</div>
+
+			<div class="row evenRow" id="contactRow">
+				<div class="container">
+				<div class="col-lg-5 center-text">
+					<h1 class="lead-header"><strong>Contact Us</strong></h1>
+					<h3><strong>tel:</strong></h3>
+					
+					<h4><a href="tel:301-613-1392"> +301-613-1392</a></h4>
+					
+					<h3><strong>fax:</strong></h3>
+					
+					<h4>+301-497-1945</h4>
+					
+					<h3><strong>email:</strong></h3>
+					
+					<h4><a href="mailto:prstech1@gmail.com"> prstech1@gmail.com</a></h4>
+					<br/>
+				</div>
+
+				<div class="col-lg-7">
+					<div class="row">
+						<form  class="center-text" action="index.php" method="post" id="messageForm">			
+							<div class="col-lg-6">
+								<input type="text"  name="firstName" placeholder="your first name" class="text-input center form-control" required>
+								<input type="text"  name="lastName" placeholder="your last name" class="text-input center form-control" required>
+							</div>
+							<div class="col-lg-6">
+								<input type="email"  name="email" placeholder="your e-mail" class="text-input center form-control" required>
+								<input type="text"  name="phoneNumber" placeholder="your phone number" class="text-input center form-control" required>
+							</div>
+		
+						</form>
+					</div>
+
+					<div class="row">
+						<div class="col-lg-12">
+							<textarea name="message" form="messageForm" cols="30" rows="10" class="text-input center form-control" placeholder="your message" required></textarea>
+
+							<br/>
+
+							<button class="btn btn-default hollow-button center" form="messageForm">Send Message</button>
+
+						</div>
+					</div>
+				</div>
+				</div>
+			</div>
+
+			<div class="row center-text oddRow" id="iconRow">
+				<i class="fa fa-linkedin social-meadia-icons-footer
+				.social-meadia-icons-footer"></i>
+				<i class="fa fa-facebook-square social-meadia-icons-footer
+				.social-meadia-icons-footer"></i>
+				<i class="fa fa-twitter social-meadia-icons-footer
+				.social-meadia-icons-footer"></i>
+			</div>
+
+			<footer class="row footer">
+				<div class="container-fluid">
+					<div class="inline-block">
+						<p>&copy Lebrun's HVAC Services LLC</p>
+					</div>
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>
+
+<!-- form message code -->
+<?php
+
+	// check to see of post array is populated
+	if(array_key_exists('firstName', $_POST) && array_key_exists('lastName', $_POST) &&
+		array_key_exists('email', $_POST) && array_key_exists('phoneNumber', $_POST) && array_key_exists('message', $_POST) ){
+		$firstName = $_POST['firstName'];	
+		$lastName = $_POST['lastName'];
+		$email = $_POST['email'];
+		$phoneNumber = $_POST['phoneNumber'];
+		$message = $_POST['message'];
+
+		$message = $message . "\r\n" . "\r\n" . $phoneNumber . "\r\n" . $email;
+
+		$to = 'prstech1@gmail.com';
+		$subject = 'AC Plumbing Masters Message';
+		$from = $firstName . " " . $lastName . " < " . $email . ">";
+
+		if(mail($to,$subject,$message,$from)){
+			echo '<script> alert("Your message has been sent successfully!") </script>';
+		}else{
+			echo '<script> alert("Error! Message could not be sent.") </script>';
+		}
+	} 
+
+?>
---
+++ js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+	// navbar toogle
+	$('#navbar-button').click(function(){
+	    $('#myNavbar').slideToggle('fast');
+	});
+
+	$('.navbar-nav li a').click(function(){
+		$('#myNavbar').slideToggle('fast');
+	});
+
+	// height adjustment for content in accordion
+    $( "#accordion" ).accordion({heightStyle:'content'});
+
+    // navbar slide to content
+    $('#servicesLink').click(function() {
+		scrollToDest('#services-lead-header');
+	});
+
+    $('#aboutLink').click(function() {
+		scrollToDest("#about-lead-header");
+	});
+
+	$('#contactLink').click(function() {
+		scrollToDest("#contactRow");
+	});
+
+	$('.navbar-brand').click(function() {
+		scrollToDest(0);
+	});
+
+	$('#certifLink').click(function() {
+		scrollToDest('#rec-header');
+	});
+
+});
+
+function scrollToDest(dest){
+	if(isNaN(dest))
+		$("html, body").animate({ scrollTop:$(dest).offset().top -50 }, "slow");
+	else
+		$("html, body").animate({ scrollTop:dest }, "slow");
+
+	return false;
+}
